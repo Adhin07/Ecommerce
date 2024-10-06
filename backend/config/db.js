daa000da035b1849dbb1d228e08eb08df4a6ab6f
abinraj561 <abinrajvalayam561@gmail.com>
2024-10-06 23:42:19 +0530

    first commit

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+const mongoose =require("mongoose")
+
+async function connectDB(){
+    try{
+        await mongoose.connect(process.env.MONGOSDB_URI)
+        console.log("connect to DB")
+
+    }catch (err){
+        console.log(err)
+    }
+    
+       
+    
+}
+
+module.exports = connectDB
---
+++ backend/routes/index.js
@@ -0,0 +1,15 @@
+
+
+  
+  const express =require('express')
+const userSignUpController = require('../controller/userSignUp')
+
+const router =express.Router()
+
+
+
+router.post("/signup",userSignUpController)
+
+
+
+module.exports = router
